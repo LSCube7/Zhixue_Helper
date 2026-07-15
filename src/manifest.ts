ec722efb466f1022e65a027a1afc22f099ed0e65
小方盒LSCube <65765136+LSCube7@users.noreply.github.com>
2026-07-15 11:58:42 +0800

    chore: prepare v2.2.1 release (#5)
    * chore: prepare v2.2.1 release
    
    * fix: derive UI version from manifest

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,7 +1,7 @@
 const manifest = {
   manifest_version: 3,
   name: "__MSG_extensionName__",
-  version: "2.2.0",
+  version: "2.2.1",
   description: "__MSG_extensionDescription__",
   default_locale: "zh_CN",
   permissions: ["downloads", "scripting", "declarativeNetRequest"],
